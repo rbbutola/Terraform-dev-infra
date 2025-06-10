420d8ba763ca0812eeb971daf985acaaf71571c9
root <root@DESKTOP-SBJBT31.localdomain>
2025-06-10 02:10:36 +0000

    deletation protect 2newadded

--- secretsmanager.tf
+++ secretsmanager.tf
@@ -1,5 +1,5 @@
 resource "aws_secretsmanager_secret" "db_secret" {
-  name = "sybil-health-dev-env1-dbsecret"
+  name = "sybil-health-dev-env2-dbsecret"
 }
 
 resource "aws_secretsmanager_secret_version" "db_secret_version" {
