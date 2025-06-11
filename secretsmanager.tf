4c75401854be1d3150256969b315985a8bfacafa
rbbutola <rbbutolabutola@gmail.com>
2025-06-11 18:34:00 +0530

    Update secretsmanager.tf

--- secretsmanager.tf
+++ secretsmanager.tf
@@ -1,5 +1,5 @@
 resource "aws_secretsmanager_secret" "db_secret" {
-  name = "sybil-health-dev-env5-dbsecret"
+  name = "sybil-health-dev-env6-dbsecret"
 }
 
 resource "aws_secretsmanager_secret_version" "db_secret_version" {
