6927a38ba8848047d70ee10941b929e55480f3ce
rbbutola <rbbutolabutola@gmail.com>
2025-06-11 12:20:20 +0530

    Update rds_cloudwatch_alarms.tf

--- rds_cloudwatch_alarms.tf
+++ rds_cloudwatch_alarms.tf
@@ -1,4 +1,16 @@
-# Alarm: Database Connections > 50
+# SNS Topic
+resource "aws_sns_topic" "sybil_health_nonprod" {
+  name = "sybil_health_nonprod"
+}
+
+# SNS Email Subscription
+resource "aws_sns_topic_subscription" "sybil_health_nonprod_email" {
+  topic_arn = aws_sns_topic.sybil_health_nonprod.arn
+  protocol  = "email"
+  endpoint  = "rbbutolabutola@gmail.com"
+}
+
+# CloudWatch Alarms
 resource "aws_cloudwatch_metric_alarm" "sybil_health_dev_rds_high_connections" {
   alarm_name          = "sybil_health_dev_rds_high_connections"
   comparison_operator = "GreaterThanThreshold"
@@ -13,9 +25,9 @@ resource "aws_cloudwatch_metric_alarm" "sybil_health_dev_rds_high_connections" {
     DBInstanceIdentifier = aws_db_instance.sybil-health-dev-database.id
   }
   treat_missing_data = "breaching"
+  alarm_actions      = [aws_sns_topic.sybil_health_nonprod.arn]
 }
 
-# Alarm: CPU Utilization > 70% for 5 minutes
 resource "aws_cloudwatch_metric_alarm" "sybil_health_dev_rds_high_cpu" {
   alarm_name          = "sybil_health_dev_rds_high_cpu"
   comparison_operator = "GreaterThanThreshold"
@@ -30,9 +42,9 @@ resource "aws_cloudwatch_metric_alarm" "sybil_health_dev_rds_high_cpu" {
     DBInstanceIdentifier = aws_db_instance.sybil-health-dev-database.id
   }
   treat_missing_data = "breaching"
+  alarm_actions      = [aws_sns_topic.sybil_health_nonprod.arn]
 }
 
-# Alarm: FreeableMemory < 2 GB
 resource "aws_cloudwatch_metric_alarm" "sybil_health_dev_rds_low_memory" {
   alarm_name          = "sybil_health_dev_rds_low_memory"
   comparison_operator = "LessThanThreshold"
@@ -41,15 +53,15 @@ resource "aws_cloudwatch_metric_alarm" "sybil_health_dev_rds_low_memory" {
   namespace           = "AWS/RDS"
   period              = 300
   statistic           = "Average"
-  threshold           = 2147483648  # 2 GB in bytes
+  threshold           = 2147483648  # 2 GB
   alarm_description   = "Freeable memory is less than 2 GB"
   dimensions = {
     DBInstanceIdentifier = aws_db_instance.sybil-health-dev-database.id
   }
   treat_missing_data = "breaching"
+  alarm_actions      = [aws_sns_topic.sybil_health_nonprod.arn]
 }
 
-# Alarm: FreeStorageSpace < 25 GB
 resource "aws_cloudwatch_metric_alarm" "sybil_health_dev_rds_low_storage" {
   alarm_name          = "sybil_health_dev_rds_low_storage_space"
   comparison_operator = "LessThanThreshold"
@@ -58,15 +70,15 @@ resource "aws_cloudwatch_metric_alarm" "sybil_health_dev_rds_low_storage" {
   namespace           = "AWS/RDS"
   period              = 300
   statistic           = "Average"
-  threshold           = 26843545600  # 25 GB in bytes
+  threshold           = 26843545600  # 25 GB
   alarm_description   = "Available storage space is less than 25 GB"
   dimensions = {
     DBInstanceIdentifier = aws_db_instance.sybil-health-dev-database.id
   }
   treat_missing_data = "breaching"
+  alarm_actions      = [aws_sns_topic.sybil_health_nonprod.arn]
 }
 
-# Alarm: Read IOPS > 1000
 resource "aws_cloudwatch_metric_alarm" "sybil_health_dev_rds_high_read_iops" {
   alarm_name          = "sybil_health_dev_rds_high_read_iops"
   comparison_operator = "GreaterThanThreshold"
@@ -81,9 +93,9 @@ resource "aws_cloudwatch_metric_alarm" "sybil_health_dev_rds_high_read_iops" {
     DBInstanceIdentifier = aws_db_instance.sybil-health-dev-database.id
   }
   treat_missing_data = "breaching"
+  alarm_actions      = [aws_sns_topic.sybil_health_nonprod.arn]
 }
 
-# Alarm: Write IOPS > 1000
 resource "aws_cloudwatch_metric_alarm" "sybil_health_dev_rds_high_write_iops" {
   alarm_name          = "sybil_health_dev_rds_high_write_iops"
   comparison_operator = "GreaterThanThreshold"
@@ -98,4 +110,5 @@ resource "aws_cloudwatch_metric_alarm" "sybil_health_dev_rds_high_write_iops" {
     DBInstanceIdentifier = aws_db_instance.sybil-health-dev-database.id
   }
   treat_missing_data = "breaching"
+  alarm_actions      = [aws_sns_topic.sybil_health_nonprod.arn]
 }
