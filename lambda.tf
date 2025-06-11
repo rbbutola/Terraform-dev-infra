6e82929e546d453f830e0483f9b2645a2950fa20
rbbutola <rbbutolabutola@gmail.com>
2025-06-11 07:48:47 +0530

    Update lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -1,8 +1,8 @@
 resource "aws_lambda_function" "sybil_health_dev_lambda" {
   function_name = "sybil_health_dev_lambda"
-  handler       = "lambda_function.handler"
-  runtime       = "nodejs18.x"
   role          = aws_iam_role.sybil_health_dev_lambda_exec_role.arn
+  runtime       = "nodejs18.x"
+  handler       = "lambda_function.handler"
 
   vpc_config {
     subnet_ids = [
@@ -13,6 +13,8 @@ resource "aws_lambda_function" "sybil_health_dev_lambda" {
       aws_security_group.sybil-health-dev-vpc-sg.id
     ]
   }
+
+  publish = false  # prevents unnecessary versioning
 
   tags = {
     Name = "sybil-health-dev-lambda"
